7d6b01da13d46b51cf0d7d2fc207bb8a123dfb09
bigbang-yt-stream <woobi11120@gmail.com>
2026-09-14 18:22:30 +0900

    Update fetch-youtube-charts.js

--- scripts/fetch-youtube-charts.js
+++ scripts/fetch-youtube-charts.js
@@ -45,6 +45,7 @@ async function fetchYouTubeCharts() {
   }
 
   const data = await response.json();
+  console.log("TOP LEVEL KEYS:", Object.keys(data));
 
   function findVideoViews(obj) {
   if (!obj || typeof obj !== "object") return null;
